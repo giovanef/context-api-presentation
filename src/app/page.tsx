7035ae3af4b71153d6b792fb47984be2ffc26fc6
Giovane Ferreira <giovaneferreira93@gmail.com>
2024-05-15 10:25:33 -0300

    feat: botão pra mostrar o loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
 import Link from "next/link";
 
+import { useLoading } from "@/contexts/loading";
+import Flex from "@/components/common/Flex";
+
+import styles from "./page.module.css";
+
 export default function Home() {
+  const { setIsLoading } = useLoading();
+
+  const handleShowLoading = () => {
+    setIsLoading({
+      status: true,
+      message: 'Tá carregando...'
+    })
+  }
+
   return (
     <div className={styles.container}>
-      <Link href={'/checkout'}>Ir para checkout</Link>
+      <Flex direction="column" gap="gapMedium">
+        <Link href={'/checkout'}>Ir para checkout</Link>
+
+        <button onClick={handleShowLoading}>Mostrar loading</button>
+      </Flex>
     </div>
   );
 }
